2f84f1f84742043530121cab4d006f0e26769016
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-06-07 18:51:26 +0300

    1

---
+++ src/cat-api.js
@@ -0,0 +1,13 @@
+const API_KEY = "live_7s9NgXPwVAFCyhFXTGhLvjUvqG9jRJJYimlUQ0Lg7AIWlTgF652Mgjs0KmKkC1fe";
+
+function fetchBreeds() {
+  
+  const url = `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`;
+  return fetch(url).then((res) => res.json());
+}
+
+function fetchCatByBreed(breedId){
+  const url = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`;
+  return fetch(url).then((res) => res.json());
+}
+export { fetchBreeds, fetchCatByBreed };
---
+++ src/index.js
@@ -0,0 +1,93 @@
+const refs = {
+    select: document.querySelector(".breed-select"),
+    divData: document.querySelector(".cat-info"),
+    loader: document.querySelector(".loader"),
+    error: document.querySelector(".error"),
+};
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import Notiflix from 'notiflix';
+import "notiflix/src/notiflix.css";
+import SlimSelect from 'slim-select';
+import "slim-select/dist/slimselect.css";
+import { Spinner } from 'spin.js';
+import 'spin.js/spin.css';
+var spinner = new Spinner().spin();
+refs.loader.appendChild(spinner.el);
+Notiflix.Notify.init({
+    position: 'center-top',
+    distance: '40px',
+    timeout: 3600000
+});
+refs.select.classList.add("invisible");
+refs.divData.classList.add("invisible");
+refs.loader.classList.add("invisible");
+startLoading(refs.select);
+fetchBreeds()
+    .then((breeds) => {
+        if (!breeds.length) throw new Error("Data not found");
+        const markup = breeds.map(createSelectOption).join('');
+        return markup;
+    })
+    .then(updateSelect)
+    .catch(onError)
+    .finally(endLoading);
+refs.select.addEventListener('change', onSelect);
+function createSelectOption({ id, name }) {
+    return `<option value="${id}">${name || "Unknown"}</option>`;
+}
+function updateSelect(markup) {
+    refs.select.innerHTML = markup;
+    new SlimSelect({
+        select: refs.select
+    });
+    refs.select.classList.remove("invisible");
+}
+function onSelect(e) {
+    startLoading(refs.divData);
+    fetchCatByBreed(e.target.value)
+        .then((cats) => {
+            if (!cats.length) throw new Error("Data not found");
+            const markup = cats.map(createCatInfo).join('');
+            return markup;
+        })
+        .then(updateInfo)
+        .catch(onError)
+        .finally(endLoading);
+}
+function createCatInfo({ url, breeds }) {
+    const { name, description, temperament } = breeds[0];
+    return `
+        <img class="cat-image" src="${url}" alt="${name || "Unknown"}">
+        <div class="cat-info-text">
+            <h2>${name || "Unknown"}</h2>
+            <p>${description || "Unknown"}</p>
+            <p><b>Temperament: </b>${temperament || "Unknown"}</p>
+        </div>`
+}
+ 
+function updateInfo(markup) {
+refs.divData.innerHTML = markup;
+refs.divData.classList.remove("invisible");
+}
+function startLoading(element) {
+element.classList.add("invisible");
+refs.loader.classList.remove("invisible");
+}
+function endLoading() {
+refs.loader.classList.add("invisible");
+}
+let errorOccurred = false;
+
+function onError() { if (!errorOccurred) { 
+        errorOccurred = true;
+        refs.loader.classList.add("invisible");
+        refs.divData.classList.add("invisible");
+        Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+    }
+}
+refs.select.addEventListener('click', () => {
+refs.error.classList.add("invisible");
+});
+refs.divData.addEventListener('click', () => {
+refs.error.classList.add("invisible");
+});
